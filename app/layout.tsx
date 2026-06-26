51e940b792b99fdb013811c979b24cf8141b1dbe
tomtam <frontend@nailedit.agency>
2026-06-26 13:29:47 +0700

    perf: run server functions near database

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,8 @@ export const metadata: Metadata = {
   description: "บันทึกและวิเคราะห์ทรัพย์สิน หนี้สิน รายรับ รายจ่ายส่วนบุคคล"
 };
 
+export const preferredRegion = "sin1";
+
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="th" suppressHydrationWarning>
